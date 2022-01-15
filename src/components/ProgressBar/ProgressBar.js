0a48018137b16cbfd4e891d1795e538d2268191a
Jonathan Tuzman <tuzmusic@gmail.com>
2022-01-15 17:01:30 -0500

    Add most border radius

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,7 +3,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 import {COLORS} from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+
+const heights = {small: 8, medium: 16, large: 24}
 
 const S = {
   Wrapper: styled.div`
@@ -13,17 +14,21 @@ const S = {
     align-items: center;
   `,
   OuterBar: styled.div`
+    height: ${p => heights[p.size]}px;
+    padding: ${p => p.size === "large" && "4px"};
     background: ${COLORS.transparentGray15};
-    height: 24px;
     width: 370px;
     left: 623px;
     top: 170px;
-    border-radius: 8px;
+    border-radius: ${p => p.size === "large" ? 8 : 4}px;
+    overflow: hidden;
   `,
   InnerBar: styled.div`
     background: ${COLORS.primary};
     width: ${p => p.value}%;
     height: 100%;
+    // TODO: it actually *approaches* 4 between, say, 98-100%
+    border-radius: 4px ${p => p.value < 100 ? "0 0" : "4px 4px" } 4px;
   `,
   Value: styled.p`
     &::after {
@@ -34,9 +39,9 @@ const S = {
 const {InnerBar, OuterBar, Value, Wrapper} = S
 const ProgressBar = ({value, size}) => {
   return <Wrapper>
-    <Value>{value}</Value>
+    {/*<Value>{value}</Value>*/}
 
-    <OuterBar  size={size}>
+    <OuterBar size={size}>
       <InnerBar value={value}/>
     </OuterBar>
   </Wrapper>
