d2c22f145da51564cbfb914c8c2fd326290dbd8b
Jonathan Tuzman <tuzmusic@gmail.com>
2022-01-15 16:50:59 -0500

    Implement VERY basic bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,11 +2,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const S = {
+  Wrapper: styled.div`
+    display: flex;
+    width: 482px;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  OuterBar: styled.div`
+    background: ${COLORS.transparentGray15};
+    height: 24px;
+    width: 370px;
+    left: 623px;
+    top: 170px;
+    border-radius: 8px;
+  `,
+  InnerBar: styled.div`
+    background: ${COLORS.primary};
+    width: ${p => p.value}%;
+    height: 100%;
+  `,
+  Value: styled.p`
+    &::after {
+      content: "%";
+    }
+  `,
+}
+const {InnerBar, OuterBar, Value, Wrapper} = S
+const ProgressBar = ({value, size}) => {
+  return <Wrapper>
+    <Value>{value}</Value>
+
+    <OuterBar  size={size}>
+      <InnerBar value={value}/>
+    </OuterBar>
+  </Wrapper>
 };
 
 export default ProgressBar;
