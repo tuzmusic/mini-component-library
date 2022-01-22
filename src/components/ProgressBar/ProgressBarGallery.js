7eea7e079ffe247a77496f1d4d8a3e71deefe184
Jonathan Tuzman <tuzmusic@gmail.com>
2022-01-22 17:31:53 -0500

    Add progress bar gallery and story

---
+++ src/components/ProgressBar/ProgressBarGallery.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import styled from "styled-components";
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const ProgressBarGallery = ({value}) => {
+  return <Wrapper>
+    <ProgressBar value={value} size={"large"}/>
+    <ProgressBar value={value} size={"medium"}/>
+    <ProgressBar value={value} size={"small"}/>
+  </Wrapper>
+};
+
+export default ProgressBarGallery;
