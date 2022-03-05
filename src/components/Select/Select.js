4ebe4ba9869e9b1d7aabedec76178d2fe9eff652
Jonathan Tuzman <tuzmusic@gmail.com>
2022-03-05 13:38:12 -0500

    Initial, incorrect, width handling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -7,8 +7,10 @@ import { getDisplayedValue } from './Select.helpers';
 
 const S = {
   Wrapper: styled.div`
+    width: min-content;
   `,
   Select: styled.select`
+    width: ${p => p.width}px;
     font-size: 16px;
     line-height: 18.76px;
     padding: 12px 16px;
@@ -17,21 +19,46 @@ const S = {
     color: ${COLORS.gray700};
     border-radius: 8px;
     transition: color 1s;
-    
+
     &:hover {
       color: black;
     }
   `,
+  WidthWrapper: styled.div`
+  `,
 }
+
+const WidthSelect = styled(S.Select)``
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  const option = children.find(({ props }) => props?.value === value)
+  const name = option.props.children
+
+  const widthRef = useRef()
+  const _width = widthRef.current && widthRef.current.clientWidth
+  // const _width = widthRef.current && getComputedStyle(widthRef.current).width
+
+  const [width, setWidth] = useState("");
+
+  useEffect(() => {
+    setWidth(_width)
+  }, [_width])
+
+  console.log(width)
+  // debugger
+
   return (
     <S.Wrapper>
-      <S.Select value={value} onChange={onChange}>
+      {width && <S.Select value={value} onChange={onChange} width={width}>
         {children}
-      </S.Select>
+      </S.Select>}
+      <S.WidthWrapper>
+        <WidthSelect value={value} onChange={onChange} ref={widthRef}>
+          <option value={value}>{name}</option>
+        </WidthSelect>
+      </S.WidthWrapper>
     </S.Wrapper>
   );
 };
