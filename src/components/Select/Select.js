95f61522da2a4104d1a2d89e09905e7cb9698db9
Jonathan Tuzman <tuzmusic@gmail.com>
2022-03-05 13:04:08 -0500

    Initial basic styling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,29 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const S = {
+  Wrapper: styled.div`
+  `,
+  Select: styled.select`
+    font-size: 16px;
+    line-height: 18.76px;
+    padding: 12px 16px;
+
+    background: ${COLORS.transparentGray15};
+    color: ${COLORS.gray700};
+    border-radius: 8px;
+  `,
+}
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <S.Wrapper>
+      <S.Select value={value} onChange={onChange}>
+        {children}
+      </S.Select>
+    </S.Wrapper>
   );
 };
 
