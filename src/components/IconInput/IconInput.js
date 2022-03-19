6bbf7be6aa7cc2a81b16c9e0186e8053fd83b96a
Jonathan Tuzman <tuzmusic@gmail.com>
2022-03-19 17:38:03 -0400

    focus wrapper

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -15,19 +15,31 @@ const sizes = {
   }
 }
 const S = {
+  FocusWrapper: styled.div`
+    :focus-within {
+      outline: solid medium Highlight;
+    }
+
+    padding: 2px;
+    width: ${p => p.width}px;
+  `,
   Wrapper: styled.div`
     border-bottom: solid 1px black;
-    width: ${p => p.width}px;
     display: flex;
     gap: 12px;
     align-items: center;
+    padding: 2px;
   `,
   Input: styled.input`
     color: ${COLORS.gray700};
     font-size: ${p => sizes[p.size].fontSize}px;
     font-weight: 700;
     border: none;
-    
+
+    :focus {
+      outline: none;
+    }
+
     &::placeholder {
       color: ${COLORS.gray500};
       font-weight: 400;
@@ -45,9 +57,11 @@ const IconInput = ({
                      size,
                      placeholder,
                    }) =>
-  <S.Wrapper width={width}>
-    <Icon id={icon} size={sizes[size].iconSize}/>
-    <S.Input size={size} placeholder={placeholder}/>
-  </S.Wrapper>;
+  <S.FocusWrapper>
+    <S.Wrapper width={width}>
+      <Icon id={icon} size={sizes[size].iconSize}/>
+      <S.Input size={size} placeholder={placeholder}/>
+    </S.Wrapper>
+  </S.FocusWrapper>
 
 export default IconInput;
